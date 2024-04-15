0eeb4fc731cd25bbeca0266f9a8d2aa96723a718
hiren timbadiya <hirentimbadiya74@gmail.com>
2024-04-15 19:40:43 +0530

    moved libraryResources in kubecli
    Signed-off-by: hiren timbadiya <hirentimbadiya74@gmail.com>

--- vars/pipeline_server.groovy
+++ vars/pipeline_server.groovy
@@ -7,16 +7,6 @@ def call(Map config = [:]){
     }
 
     withCredentials([string(credentialsId: 'dockerhub', variable: 'dockerhub_password')]) {
-        def chartDir = "helmChart-Server"
-        def chart = libraryResource "helmChart-Server/Chart.yaml"
-        def deployment = libraryResource "helmChart-Server/templates/deployment.yaml"
-        def service = libraryResource "helmChart-Server/templates/service.yaml"
-        
-
-        writeFile file: "./helmChart-Server/Chart.yaml", text: chart
-        writeFile file: "./helmChart-Server/templates/deployment.yaml", text: deployment
-        writeFile file: "./helmChart-Server/templates/service.yaml", text: service
-
         sh """#!/bin/bash
         cd server
         docker build -t ${config.imageName} .
@@ -29,7 +19,17 @@ def call(Map config = [:]){
     }
 
      withKubeConfig(caCertificate: '', clusterName: 'gke_gcp-learning-417116_us-central1-c_main-cluster', contextName: '', credentialsId: 'k8s-cred', namespace: 'jenkins', restrictKubeConfigAccess: true, serverUrl: 'https://35.226.254.176') {
-        bat """
+        def chartDir = "helmChart-Server"
+        def chart = libraryResource "helmChart-Server/Chart.yaml"
+        def deployment = libraryResource "helmChart-Server/templates/deployment.yaml"
+        def service = libraryResource "helmChart-Server/templates/service.yaml"
+        
+
+        writeFile file: "./helmChart-Server/Chart.yaml", text: chart
+        writeFile file: "./helmChart-Server/templates/deployment.yaml", text: deployment
+        writeFile file: "./helmChart-Server/templates/service.yaml", text: service
+
+        sh """#!/bin/bash
         cd server
         DIGEST=\$(docker image inspect --format='{{index .RepoDigests 0}}' ${config.imageName} | awk -F@ '{print \$2}') 
         echo \$DIGEST
